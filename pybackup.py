46b2cf3fc63a9c3da578f875b60f378172e9773a
me <me@noreply.com>
2021-09-26 22:36:02 +0200

    added statistics and flag files

--- pybackup.py
+++ pybackup.py
@@ -16,6 +16,10 @@ import yaml
 
 cfg = {'log': 'pybackup.log', 'db': 'pybackup.db', 'split': 5,
        'max_age': 300,
+       'exclude_flag': '.bkexclude',
+       'email': { 'server': 'smtp.online.no',
+                  'from': 'backup@mysystem.com',
+                  'to': 'boss@mysystem.com'},
        'backup': [], 'exclude': []}  # read in from first argument yaml file
 db_conn: sqlite3.Connection = None
 """Database connection """
@@ -30,6 +34,16 @@ tar_file: str = 'pybackup.tar.xz'
 vol_num = 1
 """current volume number"""
 db_lock=threading.Lock()
+cnt_excluded=0
+cnt2recent=0
+cnt_same_old=0
+cnt_permission=0
+cnt_incremental=0
+cnt_cyclic=0
+cnt_flagged_exc=0
+cnt_backed_up=0
+cnt_removed=0
+error_list: list[str]=[]
 
 def prep_database():
     """
@@ -62,64 +76,74 @@ def prep_database():
         vol_num = row[0]+1
 
 
-def incremental(fullname: str, dev: int):
-    global excludes, tar_file, tar_proc
-    logging.debug('incremental: '+fullname)
+def do_incremental(fullname: str, dev: int):
+    global excludes, tar_file, tar_proc, cnt_excluded,cnt2recent,cnt_same_old,cnt_permission,cnt_incremental
+    # logging.debug('incremental: '+fullname)
     for pt in excludes:
         m = pt.search(fullname)
         if m is not None:
-            logging.debug('excluded: ' + fullname)
+            # logging.debug('excluded: ' + fullname)
+            cnt_excluded+=1
             return
     if fullname == cfg['db']:
-        logging.debug('got db name: ' + fullname)
+        # logging.debug('got db name: ' + fullname)
         return
     if fullname == tar_file:
-        logging.debug('got tarfile: ' + fullname)
+        # logging.debug('got tarfile: ' + fullname)
         return
     statbuf = os.lstat(fullname)
     if statbuf.st_dev != dev:
-        logging.debug('on different fs: ' + fullname)
+        # logging.debug('on different fs: ' + fullname)
         return
     if stat.S_ISSOCK(statbuf.st_mode):
-        logging.debug('not saving socket: '+fullname)
+        # logging.debug('not saving socket: '+fullname)
         return
     mtime = int(statbuf.st_mtime)
     if mtime > max_age:
-        logging.debug('too recent: ' + fullname)
+        # logging.debug('too recent: ' + fullname)
+        cnt2recent+=1
         return
     # checking age against database
     row = db_conn.execute('select mtime from files where name=?', (fullname,)).fetchone()
     if row is not None:
         if row[0] == mtime:
-            logging.debug('same old file: ' + fullname)
+            # logging.debug('same old file: ' + fullname)
+            cnt_same_old+=1
             return
     if not os.access(fullname,os.R_OK):
         logging.warning('missing permissions: '+fullname)
+        cnt_permission+=1
         return
-    logging.debug(f'backing up: {fullname}')
+    # logging.debug(f'backing up: {fullname}')
+    cnt_incremental+=1
     print(fullname[1:], file=tar_proc.stdin)
 
-def removeFile(fullname: str):
+def remove_file(fullname: str):
+    global cnt_removed
     with db_lock:
         try:
             db_conn.execute('delete from files where name=?',(fullname,))
             db_conn.commit()
+            cnt_removed+=1
         except Exception as ex:
             logging.error(f'exception {ex}')
 
-def cyclic(fullname: str, vol: int):
+def do_cyclic(fullname: str, vol: int):
+    global cnt_cyclic
     try:
         statbuf=os.lstat(fullname)
-        logging.debug(f'cyclic backup {fullname}({vol}')
+        # logging.debug(f'cyclic backup {fullname}({vol})')
+        cnt_cyclic+=1
         print(fullname[1:], file=tar_proc.stdin)
     except FileNotFoundError:
-        removeFile(fullname)
+        remove_file(fullname)
     except Exception as ex:
         logging.error(f'exception: {ex}')
 
 
 def do_backup():
-    global max_age
+    global max_age, cnt_flagged_exc
+    blacklist={}
     try:
         logging.debug('starting incremental backup')
         max_age = time.time() - cfg['max_age']
@@ -128,11 +152,29 @@ def do_backup():
             dev = statbuf.st_dev
             for path, dirs, files in os.walk(bl):
                 for item in files:
+                    if item == cfg['exclude_flag']:
+                        blacklist[path]=True
                     fullname = os.path.join(path, item)
-                    incremental(fullname, dev)
+                    backup=True
+                    for bs in blacklist:
+                        if fullname.startswith(bs):
+                            backup=False
+                    if backup:
+                        do_incremental(fullname, dev)
+                    else:
+                        cnt_flagged_exc+=1
                 for item in dirs:
+                    if path in blacklist:
+                        continue
                     fullname = os.path.join(path, item)+os.path.sep
-                    incremental(fullname,dev)
+                    backup=True
+                    for bs in blacklist:
+                        if fullname.startswith(bs):
+                            backup=False
+                    if backup:
+                        do_incremental(fullname, dev)
+                    else:
+                        cnt_flagged_exc+=1
         logging.debug('starting cyclic backup')
         row=db_conn.execute('select count(*) from files').fetchone()
         if row is not None:
@@ -143,8 +185,16 @@ def do_backup():
             row=rs.fetchone()
             if row is None:
                 break
-            logging.debug(f'cyclic: {row[0]} from volume {row[1]}')
-            cyclic(row[0],row[1])
+            # logging.debug(f'cyclic: {row[0]} from volume {row[1]}')
+            backup = True
+            for bs in blacklist:
+                if row[0].startswith(bs):
+                    backup = False
+            if backup:
+                do_cyclic(row[0], row[1])
+            else:
+                remove_file(row[0])
+                cnt_flagged_exc+=1
     except Exception as e:
         logging.error('exception in do_backup %s',e)
         exit(2)
@@ -154,8 +204,8 @@ def do_backup():
 
 
 def handle_finished():
-    global db_lock,vol_num
-    logging.debug('reading output')
+    global db_lock,vol_num, cnt_backed_up
+    logging.debug('reading tar output')
     try:
         while True:
             line = tar_proc.stdout.readline()
@@ -167,18 +217,21 @@ def handle_finished():
             with db_lock:
                 db_conn.execute('replace into files(name,mtime,volume) values(?,?,?)',(line,mtime,vol_num))
                 db_conn.commit()
+                cnt_backed_up+=1
     except Exception as ex:
         print('exception in handle_finish: %s',ex)
     logging.debug('reading tar output stopped')
 
 
 def handle_errors():
+    logging.debug('reading tar errors')
     while True:
         line = tar_proc.stderr.readline()
         if not line:
             break
         line = line.strip()
         print('stderr ' + line)
+        error_list.append(line)
     logging.debug('reading tar errors stopped')
 
 
@@ -194,9 +247,12 @@ def main():
     tar_file = sys.argv[2]
     with open(cfg_file) as cf:
         cfg.update(yaml.safe_load(cf))
-    pprint.pprint(cfg)
+    # pprint.pprint(cfg)
+    print(f'current configuration is:')
+    yaml.safe_dump(cfg,sys.stdout)
     logging.basicConfig(filename=cfg['log'], level=logging.DEBUG, filemode='w',
                         format='%(asctime)s [%(levelname)s] %(pathname)s:%(lineno)d %(funcName)s:\t%(message)s')
+    print(f'-----')
     logging.debug("pybackup started")
     for pt in cfg['exclude']:
         cpt = re.compile(pt)
@@ -215,11 +271,22 @@ def main():
             exec.submit(handle_finished)
             exec.submit(handle_errors)
         for row in db_conn.execute('select b.num,b.tarfile, count(f.name) from backup as b left join'
-            +' files as f on b.num=f.volume group by b.num'):
+                                   +' files as f on b.num=f.volume group by b.num'):
             if int(row[2])== 0:
                 logging.info(f'tarfile {row[1]} from backup {row[0]} can be deleted')
                 db_conn.execute('delete from backup where num=?',(row[0],))
                 db_conn.commit()
+    print(f"""\
+The results are:
+  backed up files: {cnt_backed_up:>6}
+      incremental: {cnt_incremental:>6}
+ skipped 2 recent: {cnt2recent:>6}
+  skipped as same: {cnt_same_old:>6}
+     skipped flag: {cnt_flagged_exc:>6}
+    skipped perm.: {cnt_permission:>6}
+           cyclic: {cnt_cyclic:>6}
+  removed from db: {cnt_removed:>6}
+""")
     logging.debug("ended")
 
 
