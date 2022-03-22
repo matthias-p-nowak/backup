a084a66011ff6b17cbdbc803c2fa77179a14d8f4
matthias-p-nowak <matth-no@online.no>
2022-03-22 17:22:37 +0100

    Too bad - the pipe closed too early

--- pybackup.py
+++ pybackup.py
@@ -13,20 +13,26 @@ import sys
 import threading
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
+from itertools import count
 from typing import BinaryIO
 
+import jinja2
 import yaml
+from setuptools.msvc import msvc14_gen_lib_options
+from twisted.python.log import msg
 
-KILO = 1000
 HEADER_SZ = 512
 config = {}
-cnt_excluded = 0
-cnt_same_old = 0
-cnt2recent = 0
-cnt_permission = 0
-cnt_incremental = 0
-cnt_backed_up = 0
-cnt_removed = 0
+counts = {
+    'backed_up': 0,
+    'cyclic': 0,
+    'excluded': 0,
+    'incremental': 0,
+    'permissions': 0,
+    'removed': 0,
+    'same_old': 0,
+    'too_recent': 0,
+}
 db_conn: sqlite3.Connection
 """Database connection """
 db_lock = threading.Lock()
@@ -55,11 +61,11 @@ resultT: |
 
      backed up files:{{ "%7d" | format(backed_up) }}
          incremental:{{ "%7d" | format(incremental) }}
+              cyclic:{{ "%7d" | format(cyclic) }}
     skipped 2 recent:{{ "%7d" | format(too_recent) }}
      skipped as same:{{ "%7d" | format(same_old) }}
-        skipped flag:{{ "%7d" | format(flagged_exc) }}
-       skipped perm.:{{ "%7d" | format(permission) }}
-              cyclic:{{ "%7d" | format(cyclic) }}
+        skipped flag:{{ "%7d" | format(excluded) }}
+       skipped perm.:{{ "%7d" | format(permissions) }}
      removed from db:{{ "%7d" | format(removed) }}
 
   Errors:
@@ -67,64 +73,8 @@ resultT: |
    {{ error }}
   {% endfor %}
   Messages:
-  {% for msg in msgs %}
-   {{ msg }}
+  {% for msg in msgs %}{{ msg }}
   {% endfor %}
-resultH: |
-  <html>
-  <head>
-  <style>
-    table {
-        border-collapse: collapse; 
-        padding: 3px;
-        border: solid thin red;
-    }
-    table td {
-        border: solid thin red;
-        padding: 3px;
-    }
-    table .tl {
-        color: green;
-        text-align: right;
-    }
-    table .tr {
-        color: blue;
-        text-align: center;
-    }
-    ul.tar-errors li {
-        color: red;
-    }
-    ul.msg li {
-        color: green;
-    }
-  </style>
-  </head>
-  <body><h1>Pybackup results</h1>
-  <h2>Counts:</h2>
-  <table>
-    <tr><td class="tl">backed up files:</td><td class="tr">{{ backed_up }}</td></tr>
-    <tr><td class="tl">incremental:</td><td class="tr">{{ incremental }}</td></tr>
-    <tr><td class="tl">skipped 2 recent:</td><td class="tr">{{ too_recent }}</td></tr>
-    <tr><td class="tl">skipped as same:</td><td class="tr">{{ same_old }}</td></tr>
-    <tr><td class="tl">skipped flag:</td><td class="tr">{{ flagged_exc }}</td></tr>
-    <tr><td class="tl">skipped perm.:</td><td class="tr">{{ permission }}</td></tr>
-    <tr><td class="tl">cyclic:</td><td class="tr">{{ cyclic }}</td></tr>
-    <tr><td class="tl">removed from db:</td><td class="tr">{{ removed }}</td></tr>
-  </table>
-  <h2>Errors from tar:</h2>
-  <ul class="tar-errors">
-  {% for error in errors %}
-   <li>{{ error | escape }}</li>
-  {% endfor %}
-  </ul>
-  <h2>Messages:</h2>
-  <ul class="msg">
-  {% for msg in msgs %}
-   <li>{{ msg | escape }} </li>
-  {% endfor %}
-  </ul>
-  </body></html>
-done: {}
 """
 """configuration as a nested dictionary"""
 error_list: list[str] = []
@@ -156,14 +106,21 @@ class SizeCheck:
             s = int(m.group(1))
             u = m.group(2)
             if u == 'k':
-                s *= KILO
-            elif u == 'm' or u == 'M':
-                s *= KILO * KILO
-            elif u == 'g' or u == 'G':
-                s *= KILO * KILO * KILO
+                s *= 1000
+            elif u == 'K':
+                s *= 1024
+            elif u == 'm':
+                s *= 1000000
+            elif u == 'M':
+                s *= 1024 * 1024
+            elif u == 'g':
+                s *= 1000 * 1000 * 1000
+            elif u == 'G':
+                s *= 1024 * 1024 * 1024
             self.target = s
         else:
-            self.target = 500 * KILO * KILO
+            self.target = 500 * 1024 * 1024
+        logging.debug(f"aiming at archive not exceeding {self.target} bytes")
 
     def reserve(self, size: int):
         nsz = size + HEADER_SZ + self.reserved
@@ -213,27 +170,32 @@ def prep_database():
 
 
 def handle_tar_stderr():
-    global error_list, tarring, tar_proc, db_lock, cnt_backed_up, set_lock
+    global error_list, tarring, tar_proc, db_lock, set_lock, counts
     while True:
         line = tar_proc.stderr.readline()
         if not line:
+            logging.debug("tar pipe closed")
             return
         line = line.strip()
-        f2 = line
-        while f2.endswith('/'):
-            f2 = f2[:-1]
+        if line.endswith('/'):
+            line = line[:-1]
+        # outcomes:
+        # 1. - directory/ - no beginning "/", but ending "/"
+        # 2. - file  - no beginning "/", no ending "/"
+        # something else
         with set_lock:
-            found = f2 in tarring
+            found = line in tarring
         if found:
             with set_lock:
-                tarring.remove(f2)
+                tarring.remove(line)
+            # adding the '/' at the beginning
             line = os.path.sep + line
             statbuf = os.lstat(line)
             mtime = int(statbuf.st_mtime)
             with db_lock:
                 db_conn.execute('replace into files(name,mtime,volume) values(?,?,?)', (line, mtime, vol_num))
                 db_conn.commit()
-                cnt_backed_up += 1
+                counts['backed_up'] += 1
         else:
             print(f"tar stderr {line}")
             error_list.append(line)
@@ -244,6 +206,7 @@ def handle_enc_errors():
     while True:
         line = enc_proc.stderr.readline()
         if not line:
+            logging.debug("encryption pipe closed")
             return
         line = line.strip()
         if len(line) == 0:
@@ -257,111 +220,120 @@ def handle_xz_errors():
     while True:
         line = xz_proc.stderr.readline()
         if not line:
+            logging.debug("compression pipe closed")
             return
         line = line.strip()
         if len(line) == 0:
             return
-
         print(f"enc stderr {line}")
         error_list.append(line)
 
 
 def remove_file(fn: str):
-    global db_conn, db_lock, cnt_removed
+    global db_conn, db_lock, counts
     with db_lock:
         try:
             db_conn.execute('delete from files where name=?', (fn,))
             db_conn.commit()
-            cnt_removed += 1
+            counts['removed'] += 1
         except Exception as ex:
             logging.error(f'exception {ex}')
 
 
 def do_incremental(fullname):
     global blacklist, cnt_excluded, excluding, config, start_device, \
-        cnt2recent, cnt_same_old, cnt_permission, cnt_incremental, tarring, target_sc, set_lock
+        counts, tarring, target_sc, set_lock
     for bl_item in blacklist:
         if fullname.startswith(bl_item):
-            cnt_excluded += 1
+            counts['excluded'] += 1
             return
+    stat_buf = os.lstat(fullname)
+    # adding additional / at the end for directory patterns
+    if stat.S_ISDIR(stat_buf.st_mode):
+        ext_fullname = fullname + '/'
+    else:
+        ext_fullname = fullname
     for pattern in excluding:
-        m = pattern.search(fullname)
+        m = pattern.search(ext_fullname)
         if m is not None:
-            cnt_excluded += 1
+            counts['excluded'] += 1
             return
+    # no need to count those
     if fullname == config['db']:
         return
     if fullname == config['target']:
         return
-    stat_buf = os.lstat(fullname)
     if stat_buf.st_dev != start_device:
         return
+    # sockets are created by running programs
     if stat.S_ISSOCK(stat_buf.st_mode):
         return
     mtime = int(stat_buf.st_mtime)
     if mtime > max_age:
-        cnt2recent += 1
+        counts['too_recent'] += 1
         return
-    if stat.S_ISDIR(stat_buf.st_mode):
-        fullname += '/'
     # checking age against database
     row = db_conn.execute('select mtime from files where name=?', (fullname,)).fetchone()
     if row is not None:
         if row[0] == mtime:
             # logging.debug('same old file: ' + fullname)
-            cnt_same_old += 1
+            counts['same_old'] += 1
             return
     if not os.access(fullname, os.R_OK):
         logging.warning('missing permissions: ' + fullname)
-        cnt_permission += 1
+        counts['permissions'] += 1
         return
     if target_sc.reserve(stat_buf.st_size):
         logging.debug(f"backing up: {fullname}")
-        cnt_incremental += 1
-        # no starting '/'
+        counts['incremental'] += 1
+        # removing the leading '/' so tar does not complain
         fullname = fullname[1:]
-        f2 = fullname
-        # no ending '/'
-        while f2.endswith('/'):
-            f2 = f2[:-1]
         with set_lock:
-            tarring.add(f2)
-        print(fullname, file=tar_proc.stdin)
-        tar_proc.stdin.flush()
+            tarring.add(fullname)
+        print(fullname, file=tar_proc.stdin, flush=True)
     else:
-        logging.debug(f"size too big for {fullname}")
+        logging.debug(f"size too big for {fullname}, skipping until next round")
 
 
 def do_cyclic(fullname: str):
-    global blacklist, excluding, tarring, tar_proc, target_sc, set_lock
+    global blacklist, excluding, tarring, tar_proc, target_sc, set_lock, counts
     try:
         for bl_item in blacklist:
             if fullname.startswith(bl_item):
-                remove_file(fullname)
-                return
-        for pattern in excluding:
-            m = pattern.search(fullname)
-            if m is not None:
+                counts['removed'] += 1
                 remove_file(fullname)
                 return
         stat_buf = os.lstat(fullname)
+        if stat.S_ISDIR(stat_buf.st_mode):
+            ext_fullname = fullname + '/'
+        else:
+            ext_fullname = fullname
+        for pattern in excluding:
+            m = pattern.search(ext_fullname)
+            if m is not None:
+                counts['removed'] += 1
+                remove_file(fullname)
+                return
         if stat.S_ISSOCK(stat_buf.st_mode):
             return
         mtime = int(stat_buf.st_mtime)
         if mtime > max_age:
+            counts['removed'] += 1
             remove_file(fullname)
             return
         if target_sc.reserve(stat_buf.st_size):
             logging.debug(f"backing up {fullname} {len(tarring)}")
+            counts['cyclic'] += 1
+            # remove leading '/' so tar does not complain
             fullname = fullname[1:]
             f2 = fullname
             while f2.endswith(os.path.sep):
                 f2 = f2[:-1]
             with set_lock:
                 tarring.add(f2)
-            print(fullname, file=tar_proc.stdin)
-            tar_proc.stdin.flush()
+            print(fullname, file=tar_proc.stdin, flush=True)
     except FileNotFoundError:
+        counts['removed'] += 1
         remove_file(fullname)
 
 
@@ -407,8 +379,7 @@ def do_backup():
         logging.error("exception", e)
         exit(2)
     finally:
-        logging.debug(f"closing tar input - {len(tarring)} unfinished")
-        tar_proc.stdin.close()
+        logging.debug(f"backup finished - {len(tarring)} unfinished")
 
 
 def main():
@@ -423,7 +394,7 @@ def main():
         -s <size> -- size of the archive file at max (<number>{k,m,M,g,G})
         -t <target> -- write archive to this file
     """
-    global config, defaultCfg, db_conn, tar_proc, enc_proc, xz_proc, target_file, target_sc
+    global config, defaultCfg, db_conn, tar_proc, enc_proc, xz_proc, target_file, target_sc, counts, tarring
     config = yaml.safe_load(defaultCfg)
     opts, arg = getopt.getopt(sys.argv[1:], 'c:t:l:dhs:')
     for opt, opt_arg in opts:
@@ -444,9 +415,9 @@ def main():
         elif opt == '-t':
             config['target'] = opt_arg
     logging.basicConfig(filename=config['log'], level=logging.DEBUG, filemode='w',
-                        format='%(asctime)s [%(levelname)s] %(pathname)s:%(lineno)d %(funcName)s:\t%(message)s')
+                        format='%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(funcName)s:\t%(message)s')
     logging.debug("pybackup started")
-    tar_args = ['tar', '-cv', '--no-recursion', '-T', '-']
+    tar_args = ['tar', '-cv', '--no-recursion','--verbatim-file-from', '-T', '-']
     enc_args = ['gpg', '-c', '--symmetric', '--batch', '--cipher-algo', 'TWOFISH', '--passphrase', config['key']]
     xz_args = ['xz', '-9']
     target_fn = config['target']
@@ -461,18 +432,59 @@ def main():
             target_sc = SizeCheck(config['max_target_size'], target_file.fileno())
             tar_proc = subprocess.Popen(tar_args, cwd='/', stdin=subprocess.PIPE, stdout=subprocess.PIPE,
                                         stderr=subprocess.PIPE,
-                                        encoding='UTF-8')
-            enc_proc = subprocess.Popen(enc_args, stdin=tar_proc.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            xz_proc = subprocess.Popen(xz_args, stdin=enc_proc.stdout, stdout=target_file, stderr=subprocess.PIPE)
+                                        encoding='UTF-8', bufsize=0)
+            enc_proc = subprocess.Popen(enc_args, stdin=tar_proc.stdout, stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE, bufsize=0)
+            xz_proc = subprocess.Popen(xz_args, stdin=enc_proc.stdout, stdout=target_file, stderr=subprocess.PIPE,
+                                       bufsize=0)
             with ThreadPoolExecutor(max_workers=7) as tpe:
                 tpe.submit(handle_tar_stderr)
                 tpe.submit(handle_enc_errors)
                 tpe.submit(handle_xz_errors)
                 do_backup()
-        logging.debug('tar file closed')
+                old_size = -1
+                new_size = 1
+                old_cnt = -1
+                new_cnt = 1
+                while old_size != new_size or old_cnt != new_cnt:
+                    old_size = new_size
+                    old_cnt = new_cnt
+                    time.sleep(5)
+                    new_size = os.fstat(target_file.fileno()).st_size
+                    new_cnt = len(tarring)
+                    logging.debug(f"new size {new_size} - tarring {new_cnt}")
+                tar_proc.stdin.close()
+                p, e = os.wait()
+                logging.debug(f"waiting returned {p} {e}")
+                logging.debug("tar proc finished")
+                tar_proc.stderr.flush()
+                tar_proc.stderr.close()
+                enc_proc.wait()
+                xz_proc.wait()
+                logging.debug("tpe ending")
+            logging.debug("threads finished")
+        logging.debug(f"tar file closed - {len(tarring)}")
+        for fn in tarring:
+            logging.debug(f" not yet {fn}")
+        for row in db_conn.execute('select b.num,b.tarfile, count(f.name) from backup as b left join'
+                                   + ' files as f on b.num=f.volume group by b.num'):
+            if int(row[2]) == 0:
+                msg_list.append(f'tarfile {row[1]} from backup {row[0]} can be deleted')
+                db_conn.execute('delete from backup where num=?', (row[0],))
+                db_conn.commit()
+    counts['errors'] = error_list
+    counts['msgs'] = msg_list
+    result_txt = config['resultT']
+    templ = jinja2.Template(result_txt)
+    result_txt = templ.render(counts)
+    logging.debug(result_txt)
 
 
 if __name__ == '__main__':
     print('pybackup started')
     atexit.register(print, 'pybackup exited')
-    main()
+    try:
+        main()
+        logging.debug("main ended normally")
+    except Exception as e:
+        logging.error(f"main had an exception", e)
